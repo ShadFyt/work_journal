3f9b27566a079fdbfca4c8666bd5e604d9ee3265
ShadFyt <doanryan10@gmail.com>
2021-09-26 18:56:17 -0500

    database setup

--- routes/jobs.py
+++ routes/jobs.py
@@ -1,19 +1,19 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+router = APIRouter(prefix="/jobs")
 
 
-@router.get("/jobs/", tags=['jobs'])
+@router.get("/", tags=['jobs'])
 async def show_all_jobs():
     return [
         {"loc": "24 ann rd"},
         {"loc": "26 lake ave"}
     ]
 
-@router.get("/jobs/1", tags=["jobs"])
+@router.get("/1", tags=["jobs"])
 async def show_job():
     return {"loc": "26 lake ave"}
 
-@router.post("/jobs/")
+@router.post("/")
 async def create_job():
     return "created new job"
